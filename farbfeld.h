c23b423969fc47759415e0db41aa48d989d854b6
k1574 <k1574@yandex.ru>
2021-07-20 08:58:31 +0500

    Basic color parsing implemented.

--- farbfeld.h
+++
@@ -1,5 +0,0 @@
-#include <inttypes.h>
-
-typedef struct FFPixel Pixel;
-struct FFPixel { uint16_t r, g, b, a; } ;
-
--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "farbfeld.h"
+#include <ff/ff.h>
+
+#define DEBUG 1
+#define D if(DEBUG)
 
 enum {
 	LineSize = 512,
@@ -10,10 +13,11 @@ enum {
 
 struct Color {
 	char *c;
-	Pixel p;
+	FFPixel p;
 } ;
 typedef struct Color Color;
 
+static Color *colors;
 
 static char *argv0;
 static FILE *infile;
@@ -28,10 +32,18 @@ usage(void)
 }
 
 void
-wrongfmt(void)
+wrongfmt(char *s)
 {
-	fprintf(stderr, "%s: wrong format\n", argv0);
+	fprintf(stderr, "%s: wrong format: %s\n", argv0, s);
 	exit(1);
+}
+
+char *
+rdline(void)
+{
+	char *ret = fgets(curline, sizeof(curline), infile);
+	printf("\"%s\", toknum = %d\n", curline, toknum(curline));
+	return ret ;
 }
 
 char *
@@ -52,9 +64,27 @@ gettok(char *buf, char *s, const int n)
 
 	while(!isspace(*s))
 		*pbuf++ = *s++ ;
-	pbuf = 0 ;
+	*pbuf = 0 ;
 		
 	return buf ;
+}
+
+int
+toknum(char *s)
+{
+	int n = 0 ;
+	char *ps = s ;
+
+	while(1){
+		while(isspace(*ps))
+			++ps;
+		if(!*ps) break;
+		while(!isspace(*ps) && *ps)
+			++ps;
+		++n;
+	}
+
+	return n ;
 }
 
 int
@@ -62,23 +92,37 @@ main(int argc, char *argv[])
 {
 	char *pstr;
 	char tokbuf[64];
-	int n;
+	int i;
 	argv0 = argv[0] ;
 	if(argc>1) usage() ;
 
 	infile = stdin ;
 
-	if(!fgets( curline, sizeof(curline), infile) )
-		wrongfmt();
+	if(!rdline() || toknum(curline) != 4)
+		wrongfmt("header");
+
 	width = atoi(gettok(tokbuf, curline, 0)) ;
 	height = atoi(gettok(tokbuf, curline, 1)) ;
 	nchars = atoi(gettok(tokbuf, curline, 2)) ;
 	ncolors = atoi(gettok(tokbuf, curline, 3)) ;
 
 	if(!(width && height && nchars && ncolors))
-		wrongfmt();
+		wrongfmt("header values");
 
-	printf("%d %d %d %d\n", width, height, nchars, ncolors);
+	colors = malloc(sizeof(Color) * ncolors) ;
+	for(i=0 ; i<ncolors ; ++i){
+		if(!rdline() || toknum(curline) != 3){
+			wrongfmt("color");
+		}
+		pstr = curline + nchars ;
+		colors[i].c = malloc(nchars) ;
+		ff_colorname_to_pixel(&colors[i].p, gettok(tokbuf, curline, 2));
+		D printf("%d %d %d %d\n",
+			colors[i].p.r,
+			colors[i].p.g,
+			colors[i].p.b,
+			colors[i].p.a);
+	}
 	
 	return 0 ;
 }
